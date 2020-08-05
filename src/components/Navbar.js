9fab03d93199b5fc3e4f05294bd207b7e0461369
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-08-05 12:41:28 -0400

    Style Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,16 @@ import Form from "./Form";
 function Navbar(props) {
 	return (
 		<nav className="navbar">
-			<div className="dive-link-home">
-				{/* Need a React Router <Link /> to "/" */}
-			</div>
-			<div className="div-link-inspiration">
-				{/* Replace <a> with a React Router <Link /> to "/inspiration" */}
-				{/* <a className="famous" href="#">
+			<ul>
+				<li className="li-home">
+					Home
+					{/* Need a React Router <Link /> to "/" */}
+				</li>
+				<li className="li-inspiration">
 					Inspiration
-				</a> */}
-			</div>
+					{/* Need a React Router <Link /> to "/inspiration" */}
+				</li>
+			</ul>
 			<div class="div-form-sign-in">
 				<p>Sign In</p>
 				<Form />
