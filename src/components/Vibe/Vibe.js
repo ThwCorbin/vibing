a0baea4ca1306d0d96158f0d0a13e2679e7a7fdf
Jeremiah <jscott4720@gmail.com>
2020-08-06 10:59:48 -0400

    added Vibe folder and Vibe.js

---
+++ src/components/Vibe/Vibe.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+
+class Vibe extends Component {
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>Words</div>
+        )
+    }
+}
+
+export default Vibe
