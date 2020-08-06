3811dcd511eb7f716014e55343971548754a7607
Jeremiah <jscott4720@gmail.com>
2020-08-05 20:24:19 -0400

    added header to signup page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,11 +1,15 @@
 import React, {Component} from "react"
 import Form from './Form.js'
+import Header from './Header'
 
 
 class SignUp extends Component {
     render() {
         return(
+            <React.Fragment>
+            <Header/>
             <Form/>
+            </React.Fragment>
         )
     }
 }
