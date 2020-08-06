f8e3465151c51a66201a7e8bddfd9d15b065fc03
Jeremiah <jscott4720@gmail.com>
2020-08-06 15:06:59 -0400

    Link to posts work

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,60 +3,65 @@ import "./Home.css";
 import Header from "../Header/Header";
 import Post from "../Post/Post";
 import Textarea from "../Textarea/Textarea";
+import { Link } from "react-router-dom";
 
 const url = `https://vibing-api.herokuapp.com/home/posts`;
 
 class Home extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			posts: [],
-			data: []
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      data: [],
+      id: "",
+    };
+  }
 
-	handleEvent = (e) => {
-		console.log(e);
-	};
+  handleEvent = (e) => {
+    console.log(e.target.id);
+    this.setState({ id: e.target.id });
+  };
 
-	getPosts = () => {
-		// * Fetch posts
-		console.log("Something fetched");
-		fetch(url)
-			.then((res) => {
-				return res.json();
-			})
-			.then((posts) => {
-				this.setState({
-					posts: posts.reverse(),
-				}); //setState
-			})
+  getPosts = () => {
+    // * Fetch posts
+    console.log("Something fetched");
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((posts) => {
+        this.setState({
+          posts: posts.reverse(),
+        }); //setState
+      })
 
-			.then(() => {}); //fetch
-	}; //getPosts
+      .then(() => {}); //fetch
+  }; //getPosts
 
-	render() {
-		//* check if posts array is defined
-		//* then build list of posts
-		let postsList;
-		if (this.state.posts) {
-			let postsArr = this.state.posts;
-			postsList = postsArr.map((post, idx) => {
-				return <Post post={post} callback={this.handleEvent} key={idx} />;
-			}); //answerList
-		}
-		return (
-			<div className="App">
-				<Header />
-				<Textarea callback={this.getPosts} />
-				<ul className="ul-posts">{postsList}</ul>
-			</div>
-		);
-	}
+  render() {
+    //* check if posts array is defined
+    //* then build list of posts
+    let postsList;
+    if (this.state.posts) {
+      let postsArr = this.state.posts;
+      postsList = postsArr.map((post, idx) => {
+        return <Post post={post} id={this.state.id} callback={this.handleEvent} key={idx} />;
+      }); //answerList
+    }
+    return (
+      <React.Fragment>
+        <div className="App">
+          <Header />
+          <Textarea callback={this.getPosts} />
+          <div className="div-posts">{postsList}</div>
+        </div>
+      </React.Fragment>
+    );
+  }
 
-	componentDidMount() {
-		this.getPosts();
-	} //componentDidMount
+  componentDidMount() {
+    this.getPosts();
+  } //componentDidMount
 }
 
 export default Home;
